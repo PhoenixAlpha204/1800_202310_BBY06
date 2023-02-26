ae7b065812c48f032a1efbb1696ea2b2a631d345
Quincy Wong <quincywong00@gmail.com>
2023-02-25 18:15:15 -0800

    added login w/ firebase + placeholder profile page

---
+++ scripts/firebaseAPI_team06.js
@@ -0,0 +1,18 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+    apiKey: "AIzaSyBkkTdqOUf3mLu_XTg-HeirP2ePAOgMuRQ",
+    authDomain: "comp1800-3b636.firebaseapp.com",
+    projectId: "comp1800-3b636",
+    storageBucket: "comp1800-3b636.appspot.com",
+    messagingSenderId: "756420661225",
+    appId: "1:756420661225:web:3827131b75a8f9277dc578"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();

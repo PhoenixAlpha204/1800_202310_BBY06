d4d7ee307c3b4a2f5af63b0b2b2d6b4aafb6a3c9
Victor Fung <98victorfung@gmail.com>
2023-03-24 01:55:03 -0700

    Removed navbar on index.html

--- scripts/read_reports.js
+++ scripts/read_reports.js
@@ -4,7 +4,7 @@ firebase.auth().onAuthStateChanged((user) => {
     var currentUser = db.collection("users").doc(user.uid); //global
 
     // the following functions are always called when someone is logged in
-    let cardTemplate = document.getElementById("hikeCardTemplate");
+    let cardTemplate = document.getElementById("reportCardTemplate");
 
     db.collection("report")
       .where("userID", "==", user.uid)
@@ -12,20 +12,17 @@ firebase.auth().onAuthStateChanged((user) => {
       .then((allReports) => {
         allReports.forEach((doc) => {
           //iterate thru each doc
-          var title = doc.data().method; // get value of the "name" key
-          var details = doc.data().description; // get value of the "details" key
-          //var hikeCode = doc.data().code; //get unique ID to each hike to be used for fetching right image
-          var blocked = doc.data().blocked; //gets the length field
+          var title = doc.data().method; // get value of the "method" key
+          var details = doc.data().description; // get value of the "description" key
+          //var image = doc.data().code; //get unique image to each report to be used for fetching right image
+          var blocked = doc.data().blocked; //gets the blocked field
           var address = doc.data().address;
           let newcard = cardTemplate.content.cloneNode(true);
 
-          //update title and text and image etc.
           newcard.querySelector(".card-title").innerHTML = title;
-          //newcard.querySelector('.card-length').innerHTML = hikeLength + "km";
           newcard.querySelector(".card-text").innerHTML = details;
-          //newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+          //newcard.querySelector('.card-image').src = `./images/${image}.jpg`; //Example: NV01.jpg
 
-          //NEW LINE: update to display length, duration, last updated
           newcard.querySelector(".card-length").innerText =
             "Address: " +
             doc.data().address +
@@ -39,48 +36,10 @@ firebase.auth().onAuthStateChanged((user) => {
           document.getElementById("report-go-here").appendChild(newcard);
         });
       });
+
   } else {
     // No user is signed in.
     console.log("No user is signed in");
     window.location.href = "login.html";
   }
 });
-
-//------------------------------------------------------------------------------
-// Input parameter is a string representing the collection we are reading from
-//------------------------------------------------------------------------------
-// function displayCardsDynamically(collection) {
-
-//   let cardTemplate = document.getElementById("hikeCardTemplate");
-
-//   db.collection(collection)
-//     .where("userID", "==", "gHB9bXqKYgbL3MMp5lxfq18tXlu2")
-//     .get()
-//     .then((allReports) => {
-//       allReports.forEach((doc) => {
-//         //iterate thru each doc
-//         var title = doc.data().method; // get value of the "name" key
-//         var details = doc.data().description; // get value of the "details" key
-//         //var hikeCode = doc.data().code; //get unique ID to each hike to be used for fetching right image
-//         var blocked = doc.data().blocked; //gets the length field
-//         let newcard = cardTemplate.content.cloneNode(true);
-
-//         //update title and text and image etc.
-//         newcard.querySelector(".card-title").innerHTML = title;
-//         //newcard.querySelector('.card-length').innerHTML = hikeLength + "km";
-//         newcard.querySelector(".card-text").innerHTML = details;
-//         //newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
-
-//         //NEW LINE: update to display length, duration, last updated
-//         newcard.querySelector(".card-length").innerText =
-//           "Blocked: " +
-//           doc.data().blocked +
-//           "\n" +
-//           "Last updated: " +
-//           doc.data().timestamp;
-
-//         document.getElementById(collection + "-go-here").appendChild(newcard);
-
-//       });
-//     });
-// }
